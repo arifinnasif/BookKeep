d6c85a0136e71b06ed9123df6fd9250e91e289cf
Sultanul Arifin <msa336@outlook.com>
2021-12-29 15:33:04 +0600

    Edits in Catalog page
    navbar, footer, and offcanvas added to a base template
    cards added to catalog
    sidebar added
    logo added
    static folder moved BASE_DIR for keeping objects that are not app specific

--- BookKeep/settings.py
+++ BookKeep/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'catalog', 'static')
+    os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
--- catalog/views.py
+++ catalog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_homepage(request):
-    return render(request, 'catalog.html')
+    context = {
+        "books" : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    }
+    return render(request, 'catalog.html', context)
