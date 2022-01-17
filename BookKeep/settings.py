b77053afef1f88962d662c4418bcf94ee99663df
Sultanul Arifin <msa336@outlook.com>
2022-01-17 09:58:56 +0600

    Django database changed to oracle
    Django now connects to oracle. On machines not having oracle installed
    locally use oracle instant client.
    Homepage contains data fetched from the BOOKKEEPER database

--- BookKeep/settings.py
+++ BookKeep/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'BookKeep.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+#        'NAME': 'ORCL',
+        'NAME': 'localhost:1521/orclpdb1.localdomain',
+        'USER': 'BOOKKEEPER',
+        'PASSWORD': '1234',
+        #'HOST': 'localhost',
+        #'PORT': '1521',
     }
 }
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
+from django.db import connection
+import random
+
+class BookAuthorModel:
+    def __init__(self, bookName, authorName):
+        self.bookName = bookName;
+        self.authorName = authorName;
+
 
 # Create your views here.
 def show_homepage(request):
+    cursor = connection.cursor()
+    sql = "SELECT B.NAME, A.NAME FROM BOOKS B LEFT OUTER JOIN WRITES W USING(ISBN) JOIN AUTHORS A USING (AUTHOR_ID)"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.close()
+
+    books = []
+
+    for r in result:
+        books.append(BookAuthorModel(r[0], r[1]))
+
+    random.shuffle(books)
+
     context = {
-        "books" : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+        "books" : books,
     }
     return render(request, 'catalog.html', context)
