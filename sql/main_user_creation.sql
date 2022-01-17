f50705cfe7f26c120789ac70e244ef6a93651718
Sultanul Arifin <msa336@outlook.com>
2022-01-17 14:21:45 +0600

    Spelling fixed and container options added

--- sql/main_user_creation.sql
+++ sql/main_user_creation.sql
@@ -23,8 +23,23 @@ step-4 : double click on BOOKKEEPER database
 
 /*
 Chnages Session type to Pluggable one
+Uncomment one of the two options based on your machine config and oracle version.
+This will later be used to connect to database.
+*/
+
+/*
+OPTION 1
+========
+Use ORCLPDB1 as SID or ORCLPDB1.localdomain
 */
 ALTER SESSION SET CONTAINER = ORCLPDB1;
+
+/*
+OPTION 2
+========
+Use ORCLPDB as SID or ORCLPDB.localdomain
+*/
+-- ALTER SESSION SET CONTAINER = ORCLPDB;
 
 /*
 Uncomment following line when you need to rerun the same script.
@@ -36,6 +51,6 @@ Uncomment following line when you need to rerun the same script.
 CREATE USER BOOKKEEPER IDENTIFIED BY 1234;
 ALTER USER BOOKKEEPER DEFAULT TABLESPACE users QUOTA UNLIMITED ON users;
 ALTER USER BOOKKEEPER TEMPORARY TABLESPACE TEMP;
-GRANT ALL PRIVILEDGES TO BOOKKEEPER;
+GRANT ALL PRIVILEGES TO BOOKKEEPER;
 
 COMMIT;
