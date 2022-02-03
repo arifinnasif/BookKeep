06db3ce7a1ca05bdb37b773a534be293c77934cb
Sultanul Arifin <msa336@outlook.com>
2022-02-03 23:18:38 +0600

    Book Edit Option Added

--- admin/urls.py
+++ admin/urls.py
@@ -3,6 +3,6 @@ from admin import views
 
 urlpatterns = [
     path('manage/', views.show_admin, name = 'admin'),
-    path('manage/customer-list/', views.AdminCustomerListView.as_view(), name = 'admin-customer-list'),
-    path('manage/book-list/', views.AdminBookListView.as_view(), name = 'admin-book-list'),
+    path('manage/customer-list/', views.AdminCustomerListView.as_view(), name = 'admin-customer-list-view'),
+    path('manage/book-list/', views.AdminBookListView.as_view(), name = 'admin-book-list-view'),
 ]
--- admin/views.py
+++ admin/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db import connection
 from django.views import View
+from BookKeep.settings import BASE_DIR
+import datetime
+import os
 
 import datetime
 
@@ -65,16 +68,21 @@ class BookInfoModel:
         if row[4] is None:
             self.releaseDate    = ""
         else:
-            self.releaseDate    = row[4].strftime('%Y-%m-%d %H:%M:%S')
+            self.releaseDate    = row[4].strftime('%Y-%m-%d')
         self.price          = row[5]
         self.pageCount      = row[6]
         self.quantity       = row[7]
         self.publisherName  = row[8]
 
+        self.authorID       = row[9]
+        self.publisherID    = row[10]
+
+
+
 class AdminBookListView(View):
     def get(self, request):
         cursor = connection.cursor()
-        sql = """SELECT ISBN, B.NAME, A.NAME, B.EDITION, B.RELEASE_DATE, B.PRICE, B.PAGE_COUNT, B.QUANTITY, P.NAME
+        sql = """SELECT ISBN, B.NAME, A.NAME, B.EDITION, B.RELEASE_DATE, B.PRICE, B.PAGE_COUNT, B.QUANTITY, P.NAME, AUTHOR_ID, PUBLISHER_ID
                 FROM BOOKS B
                 LEFT OUTER JOIN WRITES USING (ISBN)
                 LEFT OUTER JOIN AUTHORS A USING (AUTHOR_ID)
@@ -89,8 +97,54 @@ class AdminBookListView(View):
         for r in result:
             bookInfo.append(BookInfoModel(r))
 
+        cursor = connection.cursor()
+        sql = """SELECT AUTHOR_ID, NAME
+                FROM AUTHORS"""
+        cursor.execute(sql)
+        authorList = cursor.fetchall()
+        cursor.close()
+
+        cursor = connection.cursor()
+        sql = """SELECT PUBLISHER_ID, NAME
+                FROM PUBLISHERS"""
+        cursor.execute(sql)
+        publisherList = cursor.fetchall()
+        cursor.close()
+
         context = {
             "bookInfo" : bookInfo,
+            "authorList" : authorList,
+            "publisherList" : publisherList,
         }
 
         return render(request, 'admin_panel_book_list.html', context)
+
+    def post(self, request):
+        post_type       = request.POST.get("post_type")
+        isbn            = request.POST.get("ISBN")
+        bookName        = request.POST.get("bookName")
+        authorID        = request.POST.get("authorID")
+        edition         = request.POST.get("edition")
+        releaseDate     = datetime.datetime.strptime(request.POST.get("releaseDate"), '%Y-%m-%d')
+        price           = request.POST.get("price")
+        pageCount       = request.POST.get("pageCount")
+        quantity        = request.POST.get("quantity")
+        publisherID     = request.POST.get("publisherID")
+        uploaded_pic    = request.FILES.get('coverPic')
+
+        if post_type == "edit":
+            cursor = connection.cursor()
+            cursor.callproc("UPDATE_BOOK_INFO",
+                                [isbn, bookName, authorID, edition, releaseDate, price, pageCount, quantity, publisherID,])
+            cursor.close()
+
+        if uploaded_pic is not None:
+            uploaded_pic_ext = uploaded_pic.name.split('.')[-1]
+            cover_pic = open(os.path.join(BASE_DIR, 'static', 'book_cover', isbn+"."+uploaded_pic_ext), 'wb')
+            for chunk in uploaded_pic.chunks():
+                cover_pic.write(chunk)
+            cover_pic.close()
+
+
+        #print(request.POST.get("test1"))
+        return redirect('admin-book-list-view')
