2911a234542a96c2479417ca6d2b63f3b7c7e837
Asif Haider <1805112@ugrad.cse.buet.ac.bd>
2022-02-05 06:00:15 +0600

    multi-valued genre query fixed

--- book_details/views.py
+++ book_details/views.py
@@ -13,31 +13,34 @@ import datetime
 
 
 class BookShortDetail:
-    def __init__(self, entry):
+    def __init__(self, entry, genre):
         self.bookName = entry[0]
         self.authorName = entry[2]
         self.price = entry[4]
+        self.type = ", ".join(genre)
+
 
 class BookLongDetail:
-    def __init__(self, entry):
+    def __init__(self, entry, genre):
         self.bookName = entry[0]
         self.isbn = entry[1]
         self.authorName = entry[2]
         if entry[3] is None:
-            self.edition = "Information Not Found"
+            self.edition = "! Information Not Found !"
         else:
             self.edition = entry[3]
         self.price = entry[4]
         if entry[5] is None:
-            self.pages = "Information Not Found"
+            self.pages = "! Information Not Found !"
         else:
             self.pages = entry[5]
         if entry[6] is None:
-            self.releaseDate = "Information Not Found"
+            self.releaseDate = "! Information Not Found! "
         else:
             self.releaseDate = entry[6].strftime("%d %B %Y")
         self.quantity = entry[7]
         self.publisher = entry[8]
+        self.type = ", ".join(genre)
 
 
 class UserBookDetailsView(View):
@@ -51,15 +54,35 @@ class UserBookDetailsView(View):
                 WHERE ISBN = %s"""
         cursor.execute(sql, [isbn])
         result = cursor.fetchall()
+
+        sql_2 = """SELECT B_TYPE
+                FROM BOOKS B
+                LEFT OUTER JOIN BOOK_TYPE BT USING (ISBN)
+                WHERE ISBN = %s"""
+        cursor.execute(sql_2, [isbn])
+        type = cursor.fetchall()
+
         cursor.close()
+
+        genre = []
+        for g in type:
+            if g is None:
+                g = "Not Found"
+            else:
+                g = ''.join(g)
+                genre.append(g)
+            
+        print(genre)
 
         bookLongInfo = []
         for r in result:
-            bookLongInfo.append(BookLongDetail(r))
+            bookLongInfo.append(BookLongDetail(r, genre))
+        # bookLongInfo.add_genre(genre)
 
         bookShortInfo = []
         for r in result:
-            bookShortInfo.append(BookShortDetail(r))
+            bookShortInfo.append(BookShortDetail(r, genre))
+        # bookShortInfo.add_genre(genre)
 
         context = {
             "bookLongInfo": bookLongInfo,
