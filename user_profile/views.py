76f960761b9dbefee15bbdbe3033bc6108097c20
Asif Haider <1805112@ugrad.cse.buet.ac.bd>
2022-02-12 00:47:23 +0600

    cart add, edit, delete functional

--- user_profile/views.py
+++ user_profile/views.py
@@ -30,7 +30,6 @@ def show_profile(request, cid):
     return render(request, 'user_profile.html', context)
 
 
-
 class BookInTheCart:
     def __init__(self, entry):
         self.isbn = entry[0]
@@ -40,13 +39,12 @@ class BookInTheCart:
         self.price = entry[4]
 
 
-
 class MyCartView(View):
     def get(self, request, cid):
 
         cursor = connection.cursor()
         sql = """SELECT ISBN, B.NAME BOOK, A.NAME AUTHOR, C.QUANTITY, B.PRICE
-                FROM CARTS C 
+                FROM CARTS C
                 LEFT OUTER JOIN BOOKS B USING (ISBN)
                 LEFT OUTER JOIN WRITES W USING (ISBN)
                 LEFT OUTER JOIN AUTHORS A USING (AUTHOR_ID)
@@ -55,11 +53,12 @@ class MyCartView(View):
         result = cursor.fetchall()
         cursor.close()
 
+        if len(result) == 0:
+            messages.error(request, 'Your Cart is Empty!')
+        
         user_cart = []
         for r in result:
-            # print(r)
             user_cart.append(BookInTheCart(r))
-
 
         username = request.session.get('username')
         usertype = request.session.get('usertype', default='guest')
@@ -72,35 +71,60 @@ class MyCartView(View):
 
         return render(request, 'user_profile_cart.html', context)
 
-    def post (self, request, cid):
+    def post(self, request, cid):
         username = str(request.session['username'])
-        try:
-            quantity = int(request.POST.get('quantity'))
-            isbn = str(request.POST.get('isbn'))
-            print(quantity, isbn)
-        except ValueError:
-            messages.error(request, 'Something went wrong. Please try again!')
-            return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
-        
-        
 
-        cursor = connection.cursor()
-        sql = """SELECT COUNT(*) FROM BOOKS WHERE ISBN = %s"""
-        cursor.execute(sql, [isbn])
-        result = cursor.fetchall()
-        result = result[0][0]
-        if result == 0:
-            messages.error(request, 'Something went wrong. Please try again!')
-        else:
-        
-            return_msg = cursor.var(str).var
-            cursor.callproc("INSERT_UPDATE_CART", [quantity, username, isbn, return_msg])
-            msg = return_msg.getvalue()   
-            messages.info(request, msg)
-            connection.commit()
-            
-            connection.close()
+        if request.POST.get('post_type') == 'edit' or request.POST.get('post_type') == 'add' :
+            try:
+                quantity = int(request.POST.get('quantity'))
+                isbn = str(request.POST.get('isbn'))
+                print(quantity, isbn)
+            except ValueError:
+                messages.error(
+                    request, 'Something went wrong. Please try again!')
+                return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
+            cursor = connection.cursor()
+            sql = """SELECT COUNT(*) FROM BOOKS WHERE ISBN = %s"""
+            cursor.execute(sql, [isbn])
+            result = cursor.fetchall()
+            result = result[0][0]
+            if result == 0:
+                messages.error(
+                    request, 'Something went wrong. Please try again!')
                 
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+            else:
+
+                return_msg = cursor.var(str).var
+                cursor.callproc("INSERT_UPDATE_CART", [
+                                quantity, username, isbn, return_msg])
+                msg = return_msg.getvalue()
+                messages.info(request, msg)
+                connection.commit()
+
+                connection.close()
+
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
+        elif request.POST.get('post_type') == 'delete':
+            try:
+                isbn = str(request.POST.get('isbn'))
+                print(isbn)
+            except ValueError:
+                messages.error(request, 'Something went wrong. Please try again!')
+                return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))  
+
+            cursor = connection.cursor()
+            sql = """DELETE 
+                    FROM CARTS
+                    WHERE CUSTOMER_ID = %s AND ISBN = %s"""
+            cursor.execute(sql, [username, isbn])
+            messages.info(request, 'Item Deleted Successfully!')
+            connection.close()
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
+
+
+        
 
 
