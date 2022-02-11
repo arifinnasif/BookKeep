519793a28e9bffed3e6644aec21a02accfe7733b
Asif Haider <1805112@ugrad.cse.buet.ac.bd>
2022-02-11 18:27:29 +0600

    cart access + editing done

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from multiprocessing.sharedctypes import Value
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.db import connection
+from django.views import View
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -19,8 +23,78 @@ def show_profile(request, cid):
 
     context = {
         "userfullname": userfullname,
-        "username" : username,
-        "usertype" : usertype,
+        "username": username,
+        "usertype": usertype,
     }
 
-    return render (request, 'user_profile.html', context)
+    return render(request, 'user_profile.html', context)
+
+
+
+class BookInTheCart:
+    def __init__(self, entry):
+        self.isbn = entry[0]
+        self.bookName = entry[1]
+        self.authorName = entry[2]
+        self.quantity = entry[3]
+        self.price = entry[4]
+
+
+
+class MyCartView(View):
+    def get(self, request, cid):
+
+        cursor = connection.cursor()
+        sql = """SELECT ISBN, B.NAME BOOK, A.NAME AUTHOR, C.QUANTITY, B.PRICE
+                FROM CARTS C 
+                LEFT OUTER JOIN BOOKS B USING (ISBN)
+                LEFT OUTER JOIN WRITES W USING (ISBN)
+                LEFT OUTER JOIN AUTHORS A USING (AUTHOR_ID)
+                WHERE C.CUSTOMER_ID = %s"""
+        cursor.execute(sql, [cid])
+        result = cursor.fetchall()
+        cursor.close()
+
+        user_cart = []
+        for r in result:
+            # print(r)
+            user_cart.append(BookInTheCart(r))
+
+
+        username = request.session.get('username')
+        usertype = request.session.get('usertype', default='guest')
+
+        context = {
+            "username": username,
+            "usertype": usertype,
+            "cart": user_cart,
+        }
+
+        return render(request, 'user_profile_cart.html', context)
+
+    def post (self, request, cid):
+        username = str(request.session['username'])
+        try:
+            quantity = int(request.POST.get('quantity'))
+            isbn = str(request.POST.get('isbn'))
+            print(quantity, isbn)
+        except ValueError:
+            messages.error(request, 'Something went wrong. Please try again!')
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+        
+
+
+        cursor = connection.cursor()
+        sql = """UPDATE CARTS 
+                SET QUANTITY = %s
+                WHERE CUSTOMER_ID = %s AND ISBN = %s"""
+        cursor.execute(sql, [quantity, username, isbn])
+         
+        messages.success(request, 'Cart edited successfully!')
+        connection.commit()
+            
+        connection.close()
+    
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
+
