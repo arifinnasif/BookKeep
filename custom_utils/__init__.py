19437996921f0ad36a2f769cad1016cdb42953b6
Sultanul Arifin <msa336@outlook.com>
2022-02-18 02:32:28 +0600

    Necessary decorators added and bug fixed in old ones

--- custom_utils/__init__.py
+++ custom_utils/__init__.py
@@ -6,7 +6,7 @@ from django.db import connection
 def check_if_authorized_manager(func):
     @wraps(func)
     def wrapped(self, request, *args, **kwargs):
-        if request.session['usertype'] != 'manager' and len(request.session['username']) == 0:
+        if request.session.get('usertype') != 'manager' or len(request.session.get('username')) == 0:
             messages.error(request, "You don't have the authorization")
             return redirect('manager-login-view')
 
@@ -21,6 +21,69 @@ def check_if_authorized_manager(func):
             messages.error(request, "You don't have the authorization")
             return redirect('manager-login-view')
 
+        return func(self, request, *args, **kwargs)
+
+    return wrapped
+
+
+def check_if_authorized_customer(func):
+    @wraps(func)
+    def wrapped(self, request, *args, **kwargs):
+        if request.session.get('usertype') != 'customer' or len(request.session.get('username')) == 0:
+            messages.error(request, "You are not logged in")
+            return redirect('customer-login-view')
+
+        username = request.session['username']
+        cursor = connection.cursor()
+        sql = "SELECT PASSWORD FROM CUSTOMERS WHERE CUSTOMER_ID=%s"
+        cursor.execute(sql, [username])
+        result = cursor.fetchall()
+        cursor.close()
+
+        if len(result) == 0:
+            messages.error(request, "Something went wrong")
+            return redirect('customer-login-view')
+
+        return func(self, request, *args, **kwargs)
+
+    return wrapped
+
+
+def check_if_anonymous(func):
+    @wraps(func)
+    def wrapped(self, request, *args, **kwargs):
+        if request.session.get('usertype') == 'customer' and len(request.session.get('username')) != 0:
+            username = request.session['username']
+            cursor = connection.cursor()
+            sql = "SELECT PASSWORD FROM CUSTOMERS WHERE CUSTOMER_ID=%s"
+            cursor.execute(sql, [username])
+            result = cursor.fetchall()
+            cursor.close()
+
+            if len(result) == 0:
+                request.session.flush()
+                messages.error(request, "Something went wrong")
+                return redirect('customer-login-view')
+
+            return redirect('homepage')
+
+        if request.session.get('usertype') == 'manager' and len(request.session.get('username')) != 0:
+            username = request.session['username']
+            cursor = connection.cursor()
+            sql = "SELECT PASSWORD FROM MANAGERS WHERE ADMIN_ID=%s"
+            cursor.execute(sql, [username])
+            result = cursor.fetchall()
+            cursor.close()
+
+            if len(result) == 0:
+                request.session.flush()
+                messages.error(request, "Something went wrong")
+                return redirect('customer-login-view')
+
+            return redirect('admin-panel')
+
+
+        request.session.flush()
         return func(self, request, *args, **kwargs)
 
     return wrapped
